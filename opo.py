b0bbaf629238eb5658e71350176438f74f1a8a55
Meghanaalla <41770175+Meghanaalla@users.noreply.github.com>
2018-08-16 14:56:02 +0530

    Update opo.py

--- opo.py
+++ opo.py
@@ -1,8 +1,8 @@
 hr1,min1=map(int,raw_input().split())
 hr2,min2=map(int,raw_input().split())
-n1=hr1*60+min1
-n2=hr2*60+min2
-diff=abs(n1-n2)
+p1=hr1*60+min1
+p2=hr2*60+min2
+diff=abs(p1-p2)
 time=diff%60
 time1=(diff-time)//60
 print time1,time
